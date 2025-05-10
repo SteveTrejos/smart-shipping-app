9ea4570291d23413556a019755a425ff9350b9f7
SteveTrejos <steventrejos@outlook.com>
2025-05-09 20:56:12 -0500

    feat: se agrega trigger de actualización para tabla users y se modifica seed de users con valor null

---
+++ src/db/migrations/triggers/update_user_trigger.ts
@@ -0,0 +1,25 @@
+import {sql} from 'bun';
+
+await sql.begin(async tx => {
+    await tx`
+        CREATE OR REPLACE FUNCTION set_users_updated_at()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.updated_at = NOW();
+            RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;
+    `;
+
+    await tx`
+        DROP TRIGGER IF EXISTS update_users ON users;
+    `;
+
+    await tx`
+        CREATE TRIGGER update_users
+        AFTER UPDATE
+        ON users
+        FOR EACH ROW
+        EXECUTE FUNCTION set_users_updated_at();
+    `;
+});
