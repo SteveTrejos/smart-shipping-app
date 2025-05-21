3c4b707be48e1417cb49842d14f660c9784ee221
SteveTrejos <steventrejos@outlook.com>
2025-05-21 16:49:03 -0500

    refactor: se optimiza el método para obtener usuario por id añadiendo código de respuesta más descriptivo

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -4,8 +4,18 @@ export class UserController{
     static async getUserById(req: Request, res: Response): Promise<void>{
         try {
             const userId = Number(req.params.userId);
-            const users = await UserModel.getUserById(userId);
-            res.status(200).json(users);
+            if (isNaN(userId)) {
+                res.status(400).json({ message: 'Invalid user ID' });
+                return;
+            }
+
+            const user = await UserModel.getUserById(userId);
+
+            if (!user) {
+                res.status(404).json({ message: 'User not found' });
+                return;
+            }
+            res.status(200).json(user);
         }catch (err: any) {
             res.status(500).json({message: err.message || 'Server error'})
         }
