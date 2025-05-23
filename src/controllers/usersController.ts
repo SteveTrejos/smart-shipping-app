81460ee38288cba390f33b6e1e5393a561fa8052
SteveTrejos <steventrejos@outlook.com>
2025-05-22 20:56:35 -0500

    feat: Agregado método para creación de envío, pendiente de solucionar posibles errores de llaves foráneas en tabla de vehiculos y tipo de embalaje

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -48,4 +48,56 @@ export class UserController{
             res.status(500).json({ message: err.message || 'Server error' });
         }
     }
+
+    static async deleteUser(req: Request, res: Response): Promise<void>{
+        try {
+            const userId = Number(req.params.userId);
+            if (!userId || isNaN(userId)) {
+            res.status(400).json({ message: 'Invalid user ID' });
+            return;
+            }
+
+            const isUserDeleted = await UserModel.deleteUser(userId);
+            if(!isUserDeleted){
+                res.status(400).json({message: `Couldn't delete the user`});
+            }
+            res.status(200).json({message: `User deleted correctly`});
+        } catch (err: any) {
+            res.status(500).json({ message: err.message || 'Server error' });
+        }
+    }
+
+    static async getAllShipments(req: Request, res: Response): Promise<void>{
+        const userId = Number(req.params.userId);
+        try {
+            if (!userId || isNaN(userId)) {
+            res.status(400).json({ message: 'Invalid user ID' });
+            return;
+            }
+
+            const shipments = await UserModel.getAllShipments(userId);
+            res.status(200).json(shipments);
+
+        } catch (err) {
+            res.status(500).json({message: `Couldn't get all the shipments for user ${userId}`})
+        }
+    }
+
+    static async createShipment(req: Request, res: Response){
+        try {
+            const params = req.body;
+            if(Object.keys(params).length === 0){
+                res.status(400).json({message: `Not parameters found in the request`});
+            }
+            const newShipment = await UserModel.createShipment(params);
+            if(!newShipment){
+                console.log(newShipment)
+                res.status(400).json({message: `Couldn't create the shipment. Unable to find it`});
+                return;
+            }
+            if(Object.keys(newShipment).length > 0) res.status(200).json({message: `Shipment created correctly`});
+        } catch (err) {
+            res.status(500).json({message: `Couldn't create the shipment. ${err}`})
+        }
+    }
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,9 @@ import { UserController } from '../controllers/usersController';
 const router = Router();
 
 router.get("/users/:userId", UserController.getUserById);
+router.get("/users/:userId/shipments", UserController.getAllShipments);
 router.patch("/users/:userId", UserController.updateUser);
+router.patch("/users/:userId/status", UserController.deleteUser);
+router.post("/users/:userId/shipments", UserController.createShipment);
 
 export default router;
