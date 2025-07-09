057542995402b570d7801d9a05d1e1e58bb4a6ac
Eliifin <elianagr0304@gmail.com>
2025-07-09 17:38:23 -0500

    feat: cors types added

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import userRouter from './routes/users';
 import authRouter from './routes/auth';
 import adminRouter from './routes/admin';
+import cors from 'cors';
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use('/', userRouter);
 app.use('/', authRouter);
 app.use('/', adminRouter);
-const bcryptNewPassword = await Bun.password.hash('Trepagamer11#', {
+const bcryptNewPassword = await Bun.password.hash('Unodostrescuatro', {
                 algorithm: 'bcrypt',
                 cost: 10
             });
